43a2433a2fccc0ac2aceacbd648deaceb47d877e
Víctor Sánchez <victor.s.olaya@gmail.com>
2017-10-08 10:22:46 +0200

    Separate de files by its type

---
+++ index.js
@@ -0,0 +1,56 @@
+//Events
+document.getElementById('calc').addEventListener('click', CalculateTotalTime);
+document.getElementById('addInputs').addEventListener('click', createPairOfInputs);
+document.getElementById('substractTime').addEventListener('click', SubstractTime);
+
+//Creation of inputs that we will use to do a minus between the later hour and the earlier hour.
+function createPairOfInputs() {
+    const container = document.getElementById('inputContainer');
+    const divFirstElement = document.createElement('div');
+    divFirstElement.className = 'inputs';
+    const divSecondElement = document.createElement('div');
+    divSecondElement.className = 'inputs';
+    const firstElement = document.createElement('input');
+    const secondElement = document.createElement('input');
+    divFirstElement.appendChild(firstElement);
+    divSecondElement.appendChild(secondElement);
+    container.appendChild(divFirstElement);
+    container.appendChild(divSecondElement);
+}
+
+//Calculate of times. Now, we plus what's inside the inputs and we put in a textarea.
+function CalculateTotalTime() {
+    const arrayTimes = [];
+    const floatTimes = Array.from(document.querySelectorAll('input'))
+        //Only take the inputs with sth inside them
+        .filter(inputValue => inputValue.value != "")
+        //We returned the values split 
+        .map(stringTime => {
+            const arrayFloat = stringTime.value.split(':');
+            const valueReturned = arrayFloat.length > 1 ? (parseFloat(arrayFloat) * 60) + parseFloat(arrayFloat[1]) : (parseFloat(arrayFloat) * 60)
+            return valueReturned;
+        })
+    //We plus all the amounts
+    const getTotal = floatTimes.reduce((valueBefore, valueAfter) => (valueBefore + valueAfter), 0);
+    //Put the value inside the textarea
+    document.getElementById('total').innerHTML = `Total :  ${getTotal}`;
+}
+
+function SubstractTime() {
+    const substractTimes = [];
+    const substraction = Array.from(document.querySelectorAll('input'))
+        .filter(inpValue => inpValue.value != "")
+        .map(stringTime => {
+            const arrayFloats = stringTime.value.split(':');
+            const valueSubstractedReturned = arrayFloats.length > 1 ? (parseFloat(arrayFloats) * 60) + parseFloat(arrayFloats[1]) : (parseFloat(arrayFloats) * 60)
+            return valueSubstractedReturned;
+        })
+    const getSubstraction = substraction.reduce((valueBefore, valueAfter) => (valueAfter - valueBefore), 0);
+    TransformToMinutes(getSubstraction);
+}
+
+function TransformToMinutes(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time - (minutes * 60);
+    document.getElementById('total').innerHTML = `Total: -->  ${minutes}:${seconds}`;
+}
